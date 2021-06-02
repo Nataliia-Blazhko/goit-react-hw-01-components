f2eb3c666a49b6ac00e1efc8304a6f2a9aa1637f
Nataliia-Blazhko <blazhko.natasha91@gmail.com>
2021-06-02 16:52:19 +0300

    добавляет решение 4 задачи и начальную стилизацию

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './styles.scss';
 import Profile from './components/profile/Profile';
 import Statistics from './components/statistics/Statistics';
 import FriendList from './components/friend-list/FriendList';
+import TransactionHistory from './components/transaction-history/TransactionHistory';
 import user from './json/user.json';
 import statisticalData from './json/statistical-data.json';
 import friends from './json/friends.json';
+import transactions from './json/transactions.json';
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       {/* <Statistics stats={statisticalData} />; */}
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
---
+++ src/components/transaction-history/TransactionHistory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './TransactionHistory.module.scss';
+
+function TransactionHistory({ items }) {
+  return (
+    <table className={styles['transaction-history']}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionHistory;
